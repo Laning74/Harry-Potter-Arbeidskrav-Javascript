bc1b8b9c6695266216e19fb37ed27b2dd022efa1
Marlatine <Martine-evensen@hotmail.com>
2022-03-30 12:49:12 +0200

    searchbar works, added status, age and house status. Also added red color to dead status

--- yearbook-searchbar.js
+++ yearbook-searchbar.js
@@ -3,19 +3,14 @@ const searchBar = document.querySelector(".searchbar");
 // const searchBtn = document.querySelector(".search-btn");
 let hpCharacters = [];
 
-searchBar.addEventListener('keyup', (e) => {
+searchBar.addEventListener("keyup", (e) => {
   const searchString = e.target.value.toLowerCase();
 
   const filteredCharacters = hpCharacters.filter((character) => {
-      return (
-          character.name.toLowerCase().includes(searchString) ||
-          character.house.toLowerCase().includes(searchString)
-      );
+    return character.name.toLowerCase().includes(searchString);
   });
   displayCharacters(filteredCharacters);
 });
-
-
 
 const loadCharacters = async () => {
   try {
@@ -30,17 +25,56 @@ const loadCharacters = async () => {
 const displayCharacters = (characters) => {
   const htmlString = characters
     .map((character) => {
-      return `
-            <li class="character">
-                <h2>${character.name}</h2>
-                <p>House: ${character.house}</p>
-                <img src="${character.image}"></img>
-            </li>
+      let age = character.yearOfBirth;
+      let placeholder = character.image;
+      if (character.image === "") {
+        placeholder = "./images/defaultimage.png";
+      }
+      let hogwartsHouse = character.house;
+      if (character.house === "") {
+        hogwartsHouse = "Not in a house";
+      }
+      let characterStatus = character.alive;
+      if (character.alive === false) {
+        characterStatus = "Is dead";
+        return `<li class="character">
+        <h2>${character.name}</h2>
+        <p class ="character-status-dead">Status: ${characterStatus}</p> 
+        <p class="character-house">House: ${hogwartsHouse}</p>
+        <img src="${placeholder}" class="character-image"/>
+        </li>
         `;
-        
+      }
+      if (age === "") {
+        characterStatus = `Is alive`;
+        return `
+        <li class="character">
+        <h2>${character.name}</h2>
+        <p class ="character-status">Status: ${characterStatus}</p> 
+        <p class="character-age">Age: Uknown</p>
+        <p class="character-house">House: ${hogwartsHouse}</p>
+        <img src="${placeholder}" class="character-image"/>
+        </li>
+        `;
+      } else {
+        characterStatus = `Is alive`;
+        return `
+        <li class="character">
+        <h2>${character.name}</h2>
+        <p class ="character-status">Status: ${characterStatus}</p> 
+        <p class="character-age">Age: ${calculateAge(age)}</p>
+        <p class="character-house">House: ${hogwartsHouse}</p>
+        <img src="${placeholder}" class="character-image"/>
+        </li>
+        `;
+      }
     })
     .join("");
   charactersList.innerHTML = htmlString;
 };
+
+function calculateAge(age) {
+  return 2022 - age;
+}
 
 loadCharacters();
