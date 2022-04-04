8721bd0e6a762732df5e650606926c7b79822043
Jomfruland <ingvild@macrell.no>
2022-04-04 20:50:43 +0200

    working with styling on card to different house

--- JS/teacherCreator.js
+++ JS/teacherCreator.js
@@ -26,6 +26,10 @@ function renderData(data) {
 // Display all staffmembers with name, house and image + delete and edit button
 
 const teacherList = document.querySelector(".teacher-list");
+const gryffindor = document.querySelector(".teacher-gryffindor");
+const slytherin = document.querySelector(".teacher-slytherin");
+const hufflepuff = document.querySelector(".teacher-hufflepuff");
+const ravenclaw = document.querySelector(".teacher-ravenclaw");
 
 const displayTeacher = (hogwartsStaffList) => {
   console.log(hogwartsStaffList);
@@ -34,17 +38,11 @@ const displayTeacher = (hogwartsStaffList) => {
     if (staffMember.image === "") {
       placeholder = "./images/defaultimage.png";
     }
+
     let hogwartsHouse = staffMember.house;
     if (staffMember.house === "") {
       hogwartsHouse = "Not in a house";
     }
-
-    function changeColor() {
-      if (staffMember.house === "Gryffindor") {
-        teacherList.style.backgroundColor = "#e6a038;";
-      }
-    }
-    changeColor();
 
     let hogwartsPatronus = staffMember.patronus;
 
@@ -59,43 +57,4 @@ const displayTeacher = (hogwartsStaffList) => {
         </li>
         `;
   });
-
-  // Hover effekt for hidden patronus denne trenger man ikke da hover ble lagd i css
-
-  //   let teachers = document.body.querySelectorAll(".teacher");
-  //   for (let teacher of teachers) {
-  //     teacher.addEventListener("mouseover", function () {
-  //       this.querySelector(".teacher-patronus").style.visibility = "visible";
-  //     });
-  //     teacher.addEventListener("mouseout", function () {
-  //       this.querySelector(".teacher-patronus").style.visibility = "hidden";
-  //     });
-  //   }
 };
-
-// function deleteTeacher(staffMember, i) {
-//   let askUser = prompt("Do you want to delete? Write yes/no");
-//   if (askUser === "yes") {
-//     staffMember.splice(i, 1);
-//   } else {
-//     alert("Nothing has been deleted");
-//   }
-//   displayTeacher(hogwartsStaffList);
-// }
-
-// deleteTeacher(hogwartsStaffList, 1);
-
-//trykk på skjerm så flytter tryllestav seg dit, samme funksjon som banan men denne funka ikke, skjønner ikke hvorfor
-
-// const imgWand = document.createElement("img");
-// imgWanda.src = "../images/Wand.png";
-// document.body.appendChild(imgWand);
-
-// document.addEventListener(
-//   "click",
-//   function (ev) {
-//     imgWand.style.transform = "translateY(" + (ev.clientY - 25) + "px)";
-//     imgWand.style.transform += "translateX(" + (ev.clientX - 25) + "px)";
-//   },
-//   false
-// );
